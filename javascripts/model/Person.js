d95edef07d4210a99bca7b1e6582571b49e00e4e
Jon Sumarto <jsumarto@csu.fullerton.edu>
2018-11-07 22:47:51 -0800

    random

--- javascripts/model/Person.js
+++ javascripts/model/Person.js
@@ -1,4 +1,24 @@
-(function () {
+(function (window) {
   'use strict';
+  var App = window.App || {};
+  var student = new Array();
+/*
+  function Person(){
+    console.log('Running the Person Function');
+    this.student = {};
+  }
+*/
 
-})();
+  function Person(fn, ln){
+    this.first_name = fn;
+    this.last_name = ln;
+  }
+
+  Person.prototype.add = function (stu) {
+    student.push(stu);
+    console.log(student);
+  };
+
+  App.Person = Person;
+  window.App = App;
+})(window);
